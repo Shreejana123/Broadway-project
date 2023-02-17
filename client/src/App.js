fa550e53c0e3e318aaea0d3087a8e7c3d15c534f
Shreejana Tandukar <shreejanatandukar2@gmail.com>
2023-02-17 21:48:33 +0545

    authentication started

--- client/src/App.js
+++ client/src/App.js
@@ -4,18 +4,26 @@ import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alert from './components/layout/Alert';
+
+import { Provider } from 'react-redux';
+import store from './store';
+
 import './App.css';
 
 const App = () =>
-  <Router>
-    <Fragment>
-      <Navbar />
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        <Navbar />
         <Routes>
           <Route path='/' element={<Landing />} />
+          <Alert />
           <Route path='/register' element={<Register />} />
           <Route path='/login' element={<Login />} />
         </Routes>
-    </Fragment>
-  </Router>
+      </Fragment>
+    </Router>
+  </Provider>
 
 export default App;
